fcd8d3500e2be47f8816b28b0da19aa9f2883ae4
Aashish <aashishdubey.dev@gmail.com>
2025-06-05 22:07:12 +0530

    FEAT: added banner component

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,13 @@
+
+export const Banner = ()=>{
+    return(
+     <div className="text-center bg-[linear-gradient(to_right,rgba(252,214,255,0.7),rgba(41,216,255,0.7),rgba(255,253,128,0.7),rgba(248,154,191,0.7),rgba(252,214,255,0.7))]">
+        <div className="cont font-medium">
+            <p>
+                <span className="hidden sm:inline">Introducing a completely redised interface - </span>
+                <a href="#" className="underline underline-offset-3">Explore the Demo</a>
+            </p>
+        </div>
+    </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+
+import { Banner } from "./components/Banner";
+
 export default function Home() {
   return (
    <div>
-    Hello world
+    <Banner />
    </div>
   );
 }
